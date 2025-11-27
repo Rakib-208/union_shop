6d9af4861afb8df7ee1ba5b1b91d3dd65a98230e
Rakib-208 <ra.hasan3210@gmail.com>
2025-11-27 03:45:59 +0000

    update homepage to static footer.

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:union_shop/buttons.dart'; // added
+import 'package:union_shop/footer.dart'; // added
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -195,77 +196,7 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-            // Footer
-            Container(
-              width: double.infinity,
-              color: Colors.grey[50],
-              padding: const EdgeInsets.all(24),
-              child: const Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Opening Hours',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  SizedBox(height: 12),
-                  Text(
-                    '❄️ Winter Break Closure Dates ❄️',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    'Closing 4pm 19/12/2025\n'
-                    'Reopening 10am 05/01/2026\n'
-                    'Last post date: 12pm on 18/12/2025',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 14,
-                    ),
-                  ),
-                  SizedBox(height: 12),
-                  SizedBox(height: 12),
-                  Text(
-                    '(Term Time)\n'
-                    'Monday - Friday 10am - 4pm\n'
-                    '(Outside of Term Time / Consolidation Weeks)\n'
-                    'Monday - Friday 10am - 3pm\n'
-                    'Purchase online 24/7',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 14,
-                    ),
-                  ),
-                  SizedBox(height: 16),
-                  Text(
-                    'Help and Information',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  SizedBox(height: 4),
-                  Text(
-                    'Terms & Conditions of Sale Policy'
-                    '  '
-                    'About Us',
-                    style: TextStyle(
-                      color: Colors.black87,
-                      fontSize: 14,
-                      decoration: TextDecoration.underline,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+            const Footer(),
           ],
         ),
       ),
