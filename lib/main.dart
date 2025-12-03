8b3609161b2d443449c58bdd577e6b3529ace929
Rakib-208 <ra.hasan3210@gmail.com>
2025-12-03 02:41:55 +0000

    setup firebase in main for logged in users.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:union_shop/firebase_options.dart';
+
 import 'package:union_shop/pages/home_page.dart';
 import 'package:union_shop/pages/product_page.dart';
 import 'package:union_shop/pages/login_page.dart';
@@ -12,7 +15,11 @@ import 'package:union_shop/pages/static/t_c.dart';
 import 'package:union_shop/pages/cart_page.dart';
 import 'package:union_shop/pages/order_history_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const UnionShopApp());
 }
 
